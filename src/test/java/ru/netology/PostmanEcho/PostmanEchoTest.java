3298597cf59b67a12e7bb999914e69b0e9c7bd89
Valeriia Kuzina <valeriiakuzina@gmail.com>
2021-03-15 15:22:43 +0300

    fail

--- src/test/java/ru/netology/PostmanEcho/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEcho/PostmanEchoTest.java
@@ -12,12 +12,12 @@ public class PostmanEchoTest {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("тестовые данные")
+                .body("Oleg")
         .when()
                 .post("/post")
         .then()
                 .statusCode(200)
-                .body("data", equalTo("тестовые данные"))
+                .body("form", equalTo("Oleg"))
         ;
     }
 }
